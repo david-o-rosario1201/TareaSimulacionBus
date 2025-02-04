8aab32f7cfbc7d478dba388582856304ad8f25eb
david-o-rosario1201 <finnnn023@gmail.com>
2025-02-04 12:19:56 -0400

    Ultimos cambios (04/02/2025)

--- TareaSimulacionBus/Program.cs
+++ TareaSimulacionBus/Program.cs
@@ -1,54 +1,172 @@
-﻿
+﻿bool detenerViaje = false;
+
+Thread keyListener = new Thread(DetectarTecla);
+keyListener.Start();
+
 Random pasajeros = new Random();
 Random desmontarPasajero = new Random();
  
 int hora = 6;
+int minutos = 0;
+
 int capacidadBus = 70;
-int pasajeroEsperando = pasajeros.Next(100);
+//int pasajeroEsperando = pasajeros.Next(100);
 int pasajeroMontado = 0;
 int pasajeroDesmontado = 0;
-Console.WriteLine(pasajeroEsperando);
-
 int[] bus = new int[70];
+string[] paradas = { "Nagua", "San Francisco de Macorís" };
+int paradaIndex = 0;
+int primerosEnSubir = 0;
 
-for(int i = 0; i < bus.Length; i++)
+
+while (!detenerViaje) // Bucle que se ejecuta hasta que se presione la barra espaciadora
 {
-    if(pasajeroEsperando > 0)
-    {
-        bus[i] = 1;
-        pasajeroMontado++;
-        pasajeroEsperando--;
-    }
+    int pasajeroEsperando = pasajeros.Next(100);
+
+    DesmontarPasajeros(ref pasajeroDesmontado);
+
+    Thread.Sleep(3000);
+
+    MontarPasajeros(ref pasajeroEsperando, ref primerosEnSubir);
+
+    paradaIndex = (paradaIndex + 1) % 2;
+
+    AumentarHora();
+
+    Thread.Sleep(2000);
 }
 
-Console.WriteLine($"\nHora: {hora}");
-Console.WriteLine($"\nParada: Nagua");
-Console.WriteLine($"\nPasajeros montados: {pasajeroMontado}");
+Console.WriteLine("\n¡Viaje terminado!");
 
-//for (int i = 0; i < bus.Length; i++)
-//{
-//    Console.WriteLine(bus[i]);
-//}
-
-for (int i = 0; i < bus.Length; i++)
+void MontarPasajeros(ref int pasajeroEsperando, ref int primerosEnSubir)
 {
-    if (bus[i] == 1)
+    int pasajerosNuevosMontados = 0;
+    Console.WriteLine($"\nHora: {hora:D2}:{minutos:D2} AM");
+    Console.WriteLine($"\nParada: {paradas[paradaIndex]}");
+    Console.WriteLine($"Pasajeros que habían en la Parada: {primerosEnSubir}");
+    Console.WriteLine($"Pasajeros nuevos: {pasajeroEsperando}");
+
+    for (int i = 0; i < bus.Length; i++)
     {
-        int demostar = desmontarPasajero.Next(0,2);
-        if(demostar > 0)
+        if (bus[i] == 0 && pasajeroMontado < capacidadBus)
         {
-            bus[i] = 0;
-            pasajeroDesmontado++;
+            if (primerosEnSubir > 0)
+            {
+                bus[i] = 1;
+                pasajeroMontado++;
+                pasajerosNuevosMontados++;
+                primerosEnSubir--;
+            }
+            else if (pasajeroEsperando > 0)
+            {
+                bus[i] = 1;
+                pasajeroMontado++;
+                pasajerosNuevosMontados++;
+                pasajeroEsperando--;
+                //if (pasajeroDesmontado > 0)
+                //    pasajeroDesmontado--;
+            }
+        }
+    }
+
+    primerosEnSubir = pasajeroEsperando;
+
+    Console.WriteLine($"Pasajeros que se desmontados: {pasajeroDesmontado}");
+    Console.WriteLine($"Pasajeros que se montados: {pasajerosNuevosMontados}");
+    Console.WriteLine($"Total de Pasajeros en el bus: {pasajeroMontado}");
+    Console.WriteLine($"Pasajeros en espera del regreso de la guagua: {primerosEnSubir}");
+    Console.WriteLine("--------------------------------------------------------------------------");
+}
+
+void DesmontarPasajeros(ref int pasajeroDesmontado)
+{
+    pasajeroDesmontado = 0;
+
+    for (int i = 0; i < bus.Length; i++)
+    {
+        if (bus[i] == 1)
+        {
+            int desmontar = desmontarPasajero.Next(0, 2);
+            if (desmontar > 0)
+            {
+                bus[i] = 0;
+                pasajeroDesmontado++;
+                if(pasajeroMontado > 0)
+                    pasajeroMontado--;
+            }
         }
     }
 }
 
-Console.WriteLine($"\n\n\nHora: {hora}");
-Console.WriteLine($"\nParada: San Francisco de Macorís");
-Console.WriteLine($"\nPasajeros desmontados: {pasajeroDesmontado}");
-Console.WriteLine($"\nPasajeros montados: {pasajeroMontado - pasajeroDesmontado}");
+void AumentarHora()
+{
+    minutos += 15;
+    if (minutos >= 60)
+    {
+        minutos -= 60;
+        hora++;
+    }
+    hora++;
+}
+void DetectarTecla()
+{
+    while (true)
+    {
+        if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Spacebar)
+        {
+            detenerViaje = true;
+            break;
+        }
+        Thread.Sleep(100);
+    }
+}
+
+
+
+
+
+
+
+//for(int i = 0; i < bus.Length; i++)
+//{
+//    if(pasajeroEsperando > 0)
+//    {
+//        bus[i] = 1;
+//        pasajeroMontado++;
+//        pasajeroEsperando--;
+//    }
+//}
+
+//Console.WriteLine($"\nHora: {hora:D2}:{minutos:D2} AM");
+//Console.WriteLine($"\nParada: Nagua");
+//Console.WriteLine($"\nPasajeros montados: {pasajeroMontado}");
+//Console.WriteLine("--------------------------------------------------------------------------");
+
+//Thread.Sleep(6000);
 
 //for (int i = 0; i < bus.Length; i++)
 //{
-//    Console.WriteLine(bus[i]);
+//    if (bus[i] == 1)
+//    {
+//        int demostar = desmontarPasajero.Next(0,2);
+//        if(demostar > 0)
+//        {
+//            bus[i] = 0;
+//            pasajeroDesmontado++;
+//        }
+//    }
 //}
+
+//minutos += 15;
+//if (minutos >= 60)
+//{
+//    minutos -= 60;
+//    hora++;
+//}
+//hora++;
+
+//Console.WriteLine($"\n\nHora: {hora:D2}:{minutos:D2} AM");
+//Console.WriteLine($"\nParada: San Francisco de Macorís");
+//Console.WriteLine($"\nPasajeros desmontados: {pasajeroDesmontado}");
+//Console.WriteLine($"\nPasajeros montados: {pasajeroMontado - pasajeroDesmontado}");
+//Console.WriteLine("--------------------------------------------------------------------------");
